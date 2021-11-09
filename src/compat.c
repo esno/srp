8a30ed7bc802743dbde4400383b38924e81e441b
Matthias Hauber <matthiashauber@googlemail.com>
2021-11-09 20:23:57 +0100

    fix indentation and code style of lua compat funcs

--- src/compat.c
+++ src/compat.c
@@ -1,20 +1,18 @@
 #include <lua.h>
 #include <lauxlib.h>
 
-#if !defined LUA_VERSION_NUM || LUA_VERSION_NUM==501
-/*
-** Adapted from Lua 5.2.0
-*/
-void luaL_setfuncs (lua_State *L, const luaL_Reg *l, int nup) {
+#if !defined LUA_VERSION_NUM || LUA_VERSION_NUM == 501
+// Adapted from Lua 5.2.0
+void luaL_setfuncs(lua_State *L, const luaL_Reg *l, int nup) {
   luaL_checkstack(L, nup+1, "too many upvalues");
-  for (; l->name != NULL; l++) {  /* fill the table with given functions */
+  for (; l->name != NULL; l++) {        // fill the table with given functions
     int i;
     lua_pushstring(L, l->name);
-    for (i = 0; i < nup; i++)  /* copy upvalues to the top */
+    for (i = 0; i < nup; i++)           // copy upvalues to the top
       lua_pushvalue(L, -(nup+1));
-    lua_pushcclosure(L, l->func, nup);  /* closure with those upvalues */
+    lua_pushcclosure(L, l->func, nup);  // closure with those upvalues
     lua_settable(L, -(nup + 3));
   }
-  lua_pop(L, nup);  /* remove upvalues */
+  lua_pop(L, nup);
 }
 #endif
--- src/include/srpcompat.h
+++ src/include/srpcompat.h
@@ -2,12 +2,12 @@
 #define _srp_compat_h 1
 
 #if LUA_VERSION_NUM < 502
-  #define luaL_newlib(L, m) \
-    (lua_newtable(L), luaL_register(L, NULL, m))
+#define luaL_newlib(L, m) \
+  (lua_newtable(L), luaL_register(L, NULL, m))
 #endif
 
-#if !defined LUA_VERSION_NUM || LUA_VERSION_NUM==501
-void luaL_setfuncs (lua_State *L, const luaL_Reg *l, int nup);
+#if !defined LUA_VERSION_NUM || LUA_VERSION_NUM == 501
+void luaL_setfuncs(lua_State *L, const luaL_Reg *l, int nup);
 #endif
 
 #endif
